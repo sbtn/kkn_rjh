962fae807faff851aa93a687396c2100eae26264
Stephen Beaton <sbtn@users.noreply.github.com>
2016-09-23 14:20:51 +0200

    Create json_Kommun1.js

---
+++ data/json_Kommun1.js
@@ -0,0 +1,15 @@
+var json_Kommun1={
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+                                                                                
+"features": [
+{ "type": "Feature", "properties": { "KNames": "Härjedalen" }, "geometry": { "type": "Point", "coordinates": [ 13.710818496459131, 62.073417084786826 ] } },
+{ "type": "Feature", "properties": { "KNames": "Berg" }, "geometry": { "type": "Point", "coordinates": [ 13.665628327963924, 62.824165691704316 ] } },
+{ "type": "Feature", "properties": { "KNames": "Åre" }, "geometry": { "type": "Point", "coordinates": [ 12.828589684489028, 63.288217531440772 ] } },
+{ "type": "Feature", "properties": { "KNames": "Krokom" }, "geometry": { "type": "Point", "coordinates": [ 13.738505905222844, 63.711403730570552 ] } },
+{ "type": "Feature", "properties": { "KNames": "Strömsund" }, "geometry": { "type": "Point", "coordinates": [ 15.462156914064808, 63.975968477855446 ] } },
+{ "type": "Feature", "properties": { "KNames": "Östersund" }, "geometry": { "type": "Point", "coordinates": [ 14.810555735275226, 63.327224490640681 ] } },
+{ "type": "Feature", "properties": { "KNames": "Ragunda" }, "geometry": { "type": "Point", "coordinates": [ 15.887255809848439, 63.211654089079452 ] } },
+{ "type": "Feature", "properties": { "KNames": "Bräcke" }, "geometry": { "type": "Point", "coordinates": [ 15.590948897992392, 62.789711557233602 ] } }
+]
+}
